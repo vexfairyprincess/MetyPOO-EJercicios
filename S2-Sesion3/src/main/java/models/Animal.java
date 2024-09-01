4acb5767c1df6415fb2baffb6bacd3b518a17864
vexfairyprincess <samora@uamv.edu.ni>
2024-08-31 20:12:25 -0600

    Re-added S2-Sesion3 as a regular folder

---
+++ S2-Sesion3/src/main/java/models/Animal.java
@@ -0,0 +1,16 @@
+package models;
+
+public class Animal {
+    public void hacerSonido(){
+        System.out.println("Sonido genérico");
+    }
+
+}
+
+/*private class Perro extends Animal{
+    @Override
+    public void hacerSonido(){
+        System.out.println("Guau guau");
+    }
+
+}*/

06c7e8526c809d7b2d6d9767bf983f61aed06815
vexfairyprincess <samora@uamv.edu.ni>
2024-09-17 18:26:54 -0600

    class main finished

--- W5-Session9/src/main/java/Main.java
+++ W5-Session9/src/main/java/Main.java
@@ -1,23 +1,50 @@
 import dao.StudentDao;
 import models.Student;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         StudentDao students = new StudentDao();
+        Scanner sc = new Scanner(System.in);
 
-        String menu;
+        String menu = "1. Agregar estudiante\n" +
+                "2. Buscar estudiante estudiante\n" +
+                "3. Mostrar estudiantes\n" +
+                "4. Salir\n" +
+                "Seleccione una opcion: ";
 
-        Student student1 = new Student("Juan", 20, "juan@gmail.com");
-        Student student2 = new Student("Maria", 20, "maria@gmail.com");
-        Student student3 = new Student("Maura", 20, "maura@gmail.com");
-        StudentDao student = new StudentDao();
-        if (student.store(student1)) {
-            System.out.println("Estudiante almacenado correctamente");
-        } else {
-            System.out.println("Ocurrio un problema al almacenar al estudiante");
+        int option = 0;
+
+        while (option != 4) {
+            System.out.println(menu);
+            option = sc.nextInt();
+            switch (option) {
+                case 1:
+                    System.out.println("Ingrese el nombre del estudiante: ");
+                    String name = sc.next();
+                    System.out.println("Ingrese la edad del estudiante: ");
+                    int age = sc.nextInt();
+                    System.out.println("Ingrese el email del estudiante: ");
+                    String email = sc.next();
+                    Student student = new Student(name, age, email);
+                    students.store(student);
+                    break;
+
+                case 2:
+                    System.out.println("Ingrese el nombre del estudiante: ");
+                    String nameToFind = sc.next();
+                    students.findByName(nameToFind);
+                    break;
+                case 3:
+                    students.showStudents();
+                    break;
+                case 4:
+                    System.out.println("Saliendo...");
+                    break;
+
+            }
+
         }
-
-        student.showStudents();
     }
-
 }
